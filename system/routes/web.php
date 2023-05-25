6b60a16a7dc3a6789e0f134a41fef72583d573bb
Junaidi <junaidiju4109@gmail.com>
2023-05-25 11:05:46 +0700

    tampilan admin

--- system/routes/web.php
+++ system/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function(){
+    include "_/admin.php";
 });
 
 Route::controller(HomeController::class)->group(function () {
@@ -35,3 +37,8 @@ Route::controller(HomeController::class)->group(function () {
     Route::get('caraorder', 'caraorder');
     // Route::get('bisnis', 'bisnis');
 });
+
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'LoginProses']);
+Route::get('logout', [AuthController::class, 'logout']);
