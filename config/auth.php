95c35932d9d4178122b686c8503d8f420460d515
Junaidi <junaidiju4109@gmail.com>
2023-05-26 09:06:39 +0700

    perubahan struktur

--- config/auth.php
+++ config/auth.php
@@ -1,5 +1,5 @@
 <?php
-use App\Models\Admin;
+
 return [
 
     /*
@@ -40,11 +40,6 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-
-        'admin' => [
-            'driver' => 'session',
-            'provider' => 'admin',
-        ],
     ],
 
     /*
@@ -68,11 +63,6 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
-        ],
-
-        'admin' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [
